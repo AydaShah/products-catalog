36b769ae07a7ed1ff99bb6f905c2696640df8135
Ayda <ayda@appson.ir>
2018-04-10 11:34:57 +0430

    init main activity

--- app/src/main/java/com/aydashah/productscatalog/app/activities/MainActivity.java
+++ app/src/main/java/com/aydashah/productscatalog/app/activities/MainActivity.java
@@ -1,15 +1,16 @@
 package com.aydashah.productscatalog.app.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
 import com.aydashah.productscatalog.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
