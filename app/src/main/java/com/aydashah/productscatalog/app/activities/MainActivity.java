f4e2cf347b7256a27f4082d888a9e2997062ab20
Ayda <ayda@appson.ir>
2018-04-10 10:59:30 +0430

    init application class

--- app/src/main/java/com/aydashah/productscatalog/app/activities/MainActivity.java
+++ app/src/main/java/com/aydashah/productscatalog/app/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.aydashah.productscatalog;
+package com.aydashah.productscatalog.app.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.aydashah.productscatalog.R;
 
 public class MainActivity extends AppCompatActivity {
 
