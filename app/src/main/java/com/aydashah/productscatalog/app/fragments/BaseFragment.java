634c06fc48165be7ac731f51c02160377ba7ef4b
Ayda <ayda@appson.ir>
2018-04-13 10:34:49 +0430

    improve classes strukture

--- app/src/main/java/com/aydashah/productscatalog/app/fragments/BaseFragment.java
+++ app/src/main/java/com/aydashah/productscatalog/app/fragments/BaseFragment.java
@@ -1,12 +1,83 @@
 package com.aydashah.productscatalog.app.fragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.aydashah.productscatalog.R;
+import com.aydashah.productscatalog.listener.AdapterListener;
+
+import retrofit2.Call;
 
 /**
  * Created by AydaShah on 4/10/18.
  */
 
-public class BaseFragment extends Fragment {
+public class BaseFragment<T> extends Fragment implements AdapterListener<T>, View.OnClickListener {
 
+    protected TextView mEmptyView;
+    protected TextView mErrorMessageTextView;
+    protected LinearLayout mNetworkProblemView;
+    protected RecyclerView mDefaultRecyclerView;
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mNetworkProblemView = view.findViewById(R.id.networkProblemView);
+        mEmptyView = view.findViewById(R.id.emptyView);
+        mErrorMessageTextView = view.findViewById(R.id.errorMessageTextView);
+        mDefaultRecyclerView = view.findViewById(R.id.defaultRecyclerView);
+
+        view.findViewById(R.id.retryButton).setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onStartLoadingList() {
+        mNetworkProblemView.setVisibility(View.GONE);
+        mEmptyView.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onEmptyResponse(T response) {
+        mEmptyView.setVisibility(View.VISIBLE);
+        mDefaultRecyclerView.setVisibility(View.GONE);
+        mNetworkProblemView.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onNoneEmptyResponse(T response) {
+        mDefaultRecyclerView.setVisibility(View.VISIBLE);
+        mNetworkProblemView.setVisibility(View.GONE);
+        mEmptyView.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onError(Call<T> call, Throwable throwable) {
+        if(!call.isCanceled()) {
+            mErrorMessageTextView.setText(throwable.getMessage());
+            mDefaultRecyclerView.setVisibility(View.GONE);
+            mNetworkProblemView.setVisibility(View.VISIBLE);
+            mEmptyView.setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.retryButton:
+                loadData();
+                break;
+        }
+    }
+
+    protected void loadData() {
+    }
+
+    protected void reloadData(){}
 }
--- app/src/main/java/com/aydashah/productscatalog/app/fragments/ProductDetailFragment.java
+++ app/src/main/java/com/aydashah/productscatalog/app/fragments/ProductDetailFragment.java
@@ -3,29 +3,18 @@ package com.aydashah.productscatalog.app.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import com.aydashah.productscatalog.R;
 import com.aydashah.productscatalog.adapter.ProductDetailListAdapter;
-import com.aydashah.productscatalog.listener.AdapterListener;
-
-import retrofit2.Call;
 
 /**
  * Created by AydaShah on 4/12/18.
  */
 
-public class ProductDetailFragment extends BaseFragment implements View.OnClickListener, AdapterListener {
-
-    private TextView mEmptyView;
-    private TextView mErrorMessageTextView;
-    private LinearLayout mNetworkProblemView;
-    private RecyclerView mProductDetailRecyclerView;
+public class ProductDetailFragment extends BaseFragment {
 
     private ProductDetailListAdapter mAdapter;
     private String mSku;
@@ -34,9 +23,8 @@ public class ProductDetailFragment extends BaseFragment implements View.OnClickL
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        super.onCreateView(inflater,container,savedInstanceState);
         View view = inflater.inflate(R.layout.fragment_product_detail, container, false);
-        initView(view);
-
         return view;
     }
 
@@ -52,8 +40,9 @@ public class ProductDetailFragment extends BaseFragment implements View.OnClickL
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        initView(view);
         mAdapter = new ProductDetailListAdapter(this, mSku);
-        mProductDetailRecyclerView.setAdapter(mAdapter);
+        mDefaultRecyclerView.setAdapter(mAdapter);
         mAdapter.load();
     }
 
@@ -63,71 +52,20 @@ public class ProductDetailFragment extends BaseFragment implements View.OnClickL
         mAdapter.cancelLoadData();
     }
 
+    @Override
     protected void loadData() {
         mAdapter.load();
     }
 
     private void initView(View view) {
-//        mNetworkProblemView = view.findViewById(R.id.networkProblemView);
-//        mEmptyView = view.findViewById(R.id.emptyView);
-//        mErrorMessageTextView = view.findViewById(R.id.errorMessageTextView);
-        mProductDetailRecyclerView = view.findViewById(R.id.productDetailRecyclerView);
-
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
         linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-        mProductDetailRecyclerView.setLayoutManager(linearLayoutManager);
-
-//        view.findViewById(R.id.retryButton).setOnClickListener(this);
-
+        mDefaultRecyclerView.setLayoutManager(linearLayoutManager);
     }
 
     @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.retryButton:
-                loadData();
-                break;
-        }
-    }
-
-
     protected void reloadData() {
         mAdapter.reload();
     }
 
-    @Override
-    public void onStartLoadingList() {
-//        mSwipeRefreshLayout.setRefreshing(true);
-//        mNetworkProblemView.setVisibility(View.GONE);
-//        mEmptyView.setVisibility(View.GONE);
-    }
-
-    @Override
-    public void onEmptyResponse(Object response) {
-//        mEmptyView.setVisibility(View.VISIBLE);
-//        mProductsRecyclerView.setVisibility(View.GONE);
-//        mNetworkProblemView.setVisibility(View.GONE);
-    }
-
-    @Override
-    public void onNoneEmptyResponse(Object response) {
-//        mSwipeRefreshLayout.setRefreshing(false);
-//        mProductsRecyclerView.setVisibility(View.VISIBLE);
-//        mNetworkProblemView.setVisibility(View.GONE);
-//        mEmptyView.setVisibility(View.GONE);
-    }
-
-    @Override
-    public void onError(Call call, Throwable throwable) {
-//        if (call.isCanceled()) {
-//            Log.d(TAG, "onError: request was cancelled");
-//        } else {
-//            Log.d(TAG, "onError: " + throwable.getMessage());
-//            mErrorMessageTextView.setText(throwable.getMessage());
-//            mSwipeRefreshLayout.setRefreshing(false);
-//            mProductsRecyclerView.setVisibility(View.GONE);
-//            mNetworkProblemView.setVisibility(View.VISIBLE);
-//            mEmptyView.setVisibility(View.GONE);
-//        }
-    }
 }
